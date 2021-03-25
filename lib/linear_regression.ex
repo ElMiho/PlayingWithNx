95a29fece8208b3930b89283e75802985cddeaa3
ElMiho <mikael.hjermitslev@gmail.com>
2021-03-25 23:02:21 +0100

    Clean-up

--- lib/linear_regression.ex
+++ lib/linear_regression.ex
@@ -4,13 +4,13 @@ defmodule PlayingWithNx.LinearRegression do
   defn calc_b(x_tensor, y_tensor) do
     top = Nx.sum(
       Nx.multiply(
-        Nx.subtract(x_tensor, Nx.mean(x_tensor)), 
-        Nx.subtract(y_tensor, Nx.mean(y_tensor))
+        (x_tensor - Nx.mean(x_tensor)), 
+        (y_tensor - Nx.mean(y_tensor))
       )
     )
     bottom = Nx.sum(
       Nx.power(
-        Nx.subtract(x_tensor, Nx.mean(x_tensor)), 
+        (x_tensor - Nx.mean(x_tensor)), 
         2
       )
     )
@@ -18,11 +18,9 @@ defmodule PlayingWithNx.LinearRegression do
   end
 
   defn calc_a(x_tensor, y_tensor) do
-    Nx.subtract(
-      Nx.mean(y_tensor), 
-      Nx.multiply(
+      Nx.mean(y_tensor) - Nx.multiply(
         calc_b(x_tensor, y_tensor), 
-        Nx.mean(x_tensor))
+        Nx.mean(x_tensor)
       )
   end
 
